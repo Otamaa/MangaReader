5455c745aa14e11d88072e45f0cbf5a600624331
Otamaa <otamaindodapuq2@gmail.com>
2025-08-28 02:47:17 +0700

    - update todos

--- MangaReader/todo.cpp
+++ MangaReader/todo.cpp
@@ -5,6 +5,5 @@
 	1. reducing the memory usage
 	2. cache the file extension
 	3. toggleable list for loaded manga folder
-	4. configuration
-	5. when program launch if the configuration has last session , show messagebox to load old session or open new session
+
 */
